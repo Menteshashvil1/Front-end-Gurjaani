4feb72744ccaa312d1f65797b9c5d19815b1649c
Menteshashvil1 <107887137+Menteshashvil1@users.noreply.github.com>
2022-08-30 17:28:32 +0400

    animation/JS

---
+++ app.js
@@ -0,0 +1,15 @@
+let Name = 'Nodari'
+let Lastname = 'Menteshashvili'
+console.log('Name/Lastname:'+ '  ' + Name + '  '  + Lastname)
+
+let address = 'Gurjaani/Velistsikhe'
+
+console.log( 'Address:' + '  ' + address)
+
+let phone = '597070962'
+
+console.log('Number:'+ '  ' + phone )
+
+let warning = 'Try to Click on Text In the BOX'
+
+alert(warning)
